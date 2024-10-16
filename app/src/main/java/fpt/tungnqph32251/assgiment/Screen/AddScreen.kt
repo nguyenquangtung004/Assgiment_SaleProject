5abb5d8f9d8a7d4123bb30627e80656debd1437a
nguyenquangtung004 <nguyenquangtung04.cv@gmail.com>
2024-10-16 10:44:24 +0700

    Đã hoàn thiện giao diện Add

--- app/src/main/java/fpt/tungnqph32251/assgiment/Screen/AddScreen.kt
+++ app/src/main/java/fpt/tungnqph32251/assgiment/Screen/AddScreen.kt
@@ -3,11 +3,15 @@ package fpt.tungnqph32251.assgiment.Screen
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import fpt.tungnqph32251.assgiment.ViewModel1.ProductViewModel
@@ -27,9 +31,17 @@ fun AddProductScreen(navController: NavHostController, productViewModel: Product
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        Text(
+            text = "Thêm Sản Phẩm Mới",  // Tiêu đề mới rõ ràng hơn
+            style = MaterialTheme.typography.h5,  // Điều chỉnh kiểu chữ
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(vertical = 16.dp)  // Tạo khoảng cách giữa tiêu đề và các thành phần khác
+        )
         // Nhập tên sản phẩm
         OutlinedTextField(
             value = name,
@@ -90,15 +102,20 @@ fun AddProductScreen(navController: NavHostController, productViewModel: Product
                     imageUrl = imageUrl,
                     price = price.toDoubleOrNull() ?: 0.0,
                     description = description,
-                    category = category
+                    category = category,
                 ) {
                     isSubmitting = false
                     navController.popBackStack() // Quay lại màn hình trước
                 }
             },
             modifier = Modifier.fillMaxWidth(),
-            enabled = !isSubmitting
-        ) {
+            enabled = !isSubmitting,
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Black, // Đặt màu nền của nút là màu đen
+                contentColor = Color.White  // Màu chữ là màu trắng để tạo sự tương phản
+            )
+        )
+        {
             Text("Thêm sản phẩm")
         }
     }
